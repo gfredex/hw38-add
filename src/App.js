95ec71d1f471f760bb3eda06da4344913f1fe8b6
Dima Zemnitski <g.fredex@gmail.com>
2024-12-01 23:15:53 +0300

    run project again

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import icon from './icon.png';
+
+const HighlightText = ({ color, children }) => {
+  return React.cloneElement(children, {
+    style: { color: color }
+  });
+};
+
+function Icon({ src, children, alt = 'image' }) {
+  return React.cloneElement(children,
+    { src: src, alt: alt, style: { height: '128px', width: '128px' }, })
+}
+
+const Accordion = ({ children }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const togglePanel = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
+  return (
+    <div className="accordion">
+      {React.Children.map(children, (child, index) => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, {
+            isActive: index === activeIndex,
+            onToggle: () => togglePanel(index),
+          });
+        }
+        return child;
+      })}
+    </div>
+  );
+};
+
+const Panel = ({ title, children, isActive, onToggle }) => {
+  return (
+    <div className="panel">
+      <div className="panel-title" onClick={onToggle}>
+        {title}
+      </div>
+      {isActive && <div className="panel-content">{children}</div>}
+    </div>
+  );
+};
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hi world</p>
+      <h3>First task</h3>
+      <HighlightText color="red">
+        <p>Текст внутри HighlightText color='red'</p>
+      </HighlightText>
+      <HighlightText color="green">
+        <p>Текст внутри HighlightText color='green'</p>
+      </HighlightText>
+      <Icon src={icon}>
+        <img />
+      </Icon>
+      <h3>Second task</h3>
+      <Accordion>
+        <Panel title="Panel 1">Content for Panel 1</Panel>
+        <Panel title="Panel 2">Content for Panel 2</Panel>
+        <Panel title="Panel 3">Content for Panel 3</Panel>
+      </Accordion>
     </div>
   );
 }
